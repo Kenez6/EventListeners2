d621cab489ad79c8c7e28bda765e28ed637fa2e9
Kenez6 <43010851+Kenez6@users.noreply.github.com>
2020-12-12 16:24:43 +0100

    Buttons2

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
-$('#button1').on('click', () => {
+$('#button1').one('click', () => {
     $( "p" ).append( "<p>You clicked button 1</p>" );
   });
-  $('#button2').on('click', () => {
+  $('#button2').one('click', () => {
     $( "#button1" ).append("is now clicked");
   });
- 
 
-function myFunction2() {
-    var myColor = document.getElementById("myColor")
+function myFunction(){
+    var myColor = document.getElementById("myColor");
     var option = myColor.options[myColor.selectedIndex].text;
     $("#button3").css('background-color', option );
-    
-}
+    }
+
+
 
 
     
